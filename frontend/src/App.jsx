0542ce074f927ab0b4991050c896d060deb54196
Alwin Yesudas <alwin.yesudas@innovaturelabs.com>
2025-07-17 17:07:07 +0530

    Added Teacher Dashboard with student view paginated functionality to view students under him as well as used redux to store and display the teacher name along with welcome message.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { Provider } from "react-redux"; 
+import { store } from "./redux/store";
 import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
+import TeacherDashboard from "./pages/TeacherDashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+
 import './App.css';
 
 /*npm install @emotion/react @emotion/styled @mui/icons-material @mui/material axios bootstrap react react-dom react-hook-form react-router-dom && npm install -D @eslint/js @types/react @types/react-dom @vitejs/plugin-react eslint eslint-plugin-react-hooks eslint-plugin-react-refresh globals vite*/
 
 function App() {
   return (
+    <Provider store={store}>
     <AuthProvider>
       <Router>
         <Routes>
@@ -22,9 +27,18 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route
+            path="/teacher/dashboard"
+            element={
+              <ProtectedRoute role="teacher">
+                <TeacherDashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
+    </Provider>
   );
 }
 
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
 import axiosInstance from "../interceptor";
+import { useDispatch } from "react-redux";
+import { setUserName, clearUserName } from "../redux/actions/userActions";
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
@@ -7,6 +9,7 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
   useEffect(() => {
     const access = localStorage.getItem("access_token");
     const refresh = localStorage.getItem("refresh_token");
@@ -22,13 +25,16 @@ export const AuthProvider = ({ children }) => {
   const login = async (credentials) => {
     try {
       const response = await axiosInstance.post("/token/", credentials);
-      const { access, refresh, role } = response.data;
+      const { access, refresh, role, full_name } = response.data;
 
       localStorage.setItem("access_token", access);
       localStorage.setItem("refresh_token", refresh);
       localStorage.setItem("role", role);
 
       setUser({ access, refresh, role });
+
+      dispatch(setUserName(full_name));
+
       return role;
     } catch (error) {
       console.error("Login failed:", error.response?.data || error.message);
@@ -41,6 +47,7 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem("refresh_token");
     localStorage.removeItem("role");
     setUser(null);
+    dispatch(clearUserName());
   };
 
   return (
