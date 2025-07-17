6d072105c442cc389fb58677254c734606306c30
Alwin Yesudas <alwin.yesudas@innovaturelabs.com>
2025-07-17 10:20:36 +0530

    Added name along with login response (i.e along wit access & refresh token)

--- backend/schoolMgmt/users/serializers.py
+++ backend/schoolMgmt/users/serializers.py
@@ -12,9 +12,11 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
         token['role'] = user.role  
+        token['full_name'] = f"{user.first_name} {user.last_name}"
         return token
 
     def validate(self, attrs):
         data = super().validate(attrs)
         data['role'] = self.user.role  
+        data['full_name'] = f"{self.user.first_name} {self.user.last_name}"
         return data
